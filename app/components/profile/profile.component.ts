9c873a820a6e7694fae5fc665d4665417f4f5c8a
Said Maadan <saidmaadan@users.noreply.github.com>
2017-01-02 11:46:25 -0600

    Created User profile, signin and signout functions

--- app/components/profile/profile.component.ts
+++ app/components/profile/profile.component.ts
@@ -7,9 +7,11 @@ import { Auth } from '../../services/auth.service';
   templateUrl: `profile.component.html`,
 })
 export class ProfileComponent  { 
-  // name = 'Angular'; 
+  profile:any;
 
   constructor(private auth: Auth){
+    this.profile = JSON.parse(localStorage.getItem('profile'));
+    console.log(this.profile);
     
   }
 }
